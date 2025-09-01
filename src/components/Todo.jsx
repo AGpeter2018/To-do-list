3061fe1e608a942d7672091ba6fae45652ffc663
AGpeter2018 <adenijipeter2018@gmail.com>
2025-09-01 19:38:35 +0100

    the filter button that was rendered was made functional

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,8 +1,11 @@
 import React from "react";
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 function Todo(props) {
   const [isEditing, setEditing] = useState(false);
+
+  const editFieldRef = useRef(null);
+  const editButtonRef = useRef(null);
 
   const [newName, setNewName] = useState("");
   function handleChange(e) {
@@ -28,6 +31,7 @@ function Todo(props) {
           type="text"
           value={newName}
           onChange={handleChange}
+          ref={editFieldRef}
         />
       </div>
       <div className="btn-group">
@@ -60,7 +64,12 @@ function Todo(props) {
         </label>
       </div>
       <div className="btn-group">
-        <button type="button" className="btn" onClick={() => setEditing(true)}>
+        <button
+          type="button"
+          className="btn"
+          onClick={() => setEditing(true)}
+          ref={editButtonRef}
+        >
           Edit <span className="visually-hidden">{props.name}</span>
         </button>
         <button
