0b5b0ffa2aa23a758ce391af0fefefb5b7f7d24f
AGpeter2018 <adenijipeter2018@gmail.com>
2025-09-01 19:06:16 +0100

    rendering of filter button to the ui

--- src/App.js
+++ src/App.js
@@ -6,8 +6,17 @@ import Todo from "./components/Todo";
 import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
 
+const FILTER_MAP = {
+  All: () => true,
+  Active: (task) => !task.completed,
+  Completed: (task) => task.completed,
+};
+
+const FILTER_NAMES = Object.keys(FILTER_MAP);
+
 function App(props) {
   const [tasks, setTasks] = useState(props.tasks);
+  const [filter, setFilter] = useState("All");
   // Toggling of a checkbox
   function toggleTaskCompleted(id) {
     const updatedTasks = tasks.map((task) => {
@@ -53,6 +62,15 @@ function App(props) {
     />
   ));
 
+  const filterList = FILTER_NAMES.map((name) => (
+    <FilterButton
+      key={name}
+      name={name}
+      isPressed={name === filter}
+      setFilter={setFilter}
+    />
+  ));
+
   function addTask(name) {
     const newTask = { id: `todo${nanoid()}`, name, completed: false };
     setTasks([...tasks, newTask]);
@@ -67,11 +85,7 @@ function App(props) {
       <h1>hello world</h1>
       <h1>TodoMatic</h1>
       <Form addTask={addTask} />
-      <div className="filters btn-group stack-exception">
-        <FilterButton />
-        <FilterButton />
-        <FilterButton />
-      </div>
+      <div className="filters btn-group stack-exception">{filterList}</div>
       <h2 id="list-heading">{headingText}</h2>
       <ul
         role="list"
